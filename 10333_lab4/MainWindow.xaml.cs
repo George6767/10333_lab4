f610e3c7c38fe82640bb07865845b6258688141a
George6767 <129250696+George6767@users.noreply.github.com>
2023-06-04 20:18:42 +0300

    00:52:09 ok

--- 10333_lab4/MainWindow.xaml.cs
+++ 10333_lab4/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace _10333_lab4
 {
@@ -20,17 +21,46 @@ namespace _10333_lab4
     /// </summary>
     public partial class MainWindow : Window
     {
-        Canvas[] cavases = new Canvas[3];
+        Canvas[] canvases = new Canvas[3];
         Hipodrome.UserControl1_Horse[] horses = new Hipodrome.UserControl1_Horse[3];
+        DispatcherTimer timer, timerUpdateSpeed;
+        Random random = new Random(); 
         public MainWindow()
         {
             InitializeComponent();
             this.ResizeMode = ResizeMode.NoResize;
             for(int i=0; i<3; i++)
             {
-                cavases[i] = new Canvas();
-                Grid.SetRow(cavases[i], i);
-                grid.Children.Add(cavases[i]);
+                canvases[i] = new Canvas();
+                Grid.SetRow(canvases[i], i);
+                grid.Children.Add(canvases[i]);
+                horses[i] = new Hipodrome.UserControl1_Horse(random.Next(20, 50));
+                canvases[i].Children.Add(horses[i]);
+            }
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(1000);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+            timerUpdateSpeed = new DispatcherTimer();
+            timerUpdateSpeed.Interval = new TimeSpan(0, 0, 2);
+            timerUpdateSpeed.Tick += new EventHandler(timertimerUpdateSpeed_Tick);
+            timerUpdateSpeed.Start();
+        }
+
+        private void timertimerUpdateSpeed_Tick(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                horses[i].UpdateSpeed(random.Next(30, 80));
+            }
+        }
+
+        private void timer_Tick(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                horses[i].XHorse += (float)horses[i].GetSpeed() / 12000f;
             }
         }
     }
